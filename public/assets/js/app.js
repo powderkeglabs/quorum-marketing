c0a2b1aa6737f5b0aba89a7b20d7401a60d53493
JJ Hiew <jj.hiew@gmail.com>
2015-12-15 23:52:07 -0500

    hook up contact us form to api

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,37 +1,58 @@
 'use strict';
 ( function(){
-  angular.module('quorumApp', ['config'])
+  angular.module('quorumApp', [])
     /// Service to call advisor list api
-    .service('Services', ['HOST', '$http', function(HOST, $http){
+    .service('Services', ['$http', function($http){
       var Services = {};
+      var location = window.location;
+      var host = '//' + location.host;
 
       // HTTP Post to submit an advisor Request
-      // Services.postRequest = function(request){
-      //   return $http.post(HOST + '/api/v1/Request', request);
-      // };
+      Services.postRequest = function(request){
+        return $http.post(host + '/api/v1/Contact/quorum', request);
+      };
 
       return Services;
     }])
 
-    .controller('requestController', ['Services', '$scope', '$timeout', function(Services, $scope, $timeout){
-      $scope.errors = false;
-      $scope.submitted = false;
+    .controller('requestController', ['Services', '$scope', '$timeout', '$location', function(Services, $scope, $timeout, $location){
+      $scope.request = {};
+      $scope.msgStatus = {
+        errors: false,
+        submitted: false,
+        success: false
+      };
 
       // Submit the request form request
-      $scope.submitRequest = function(){
-        console.log("Submot");
+      $scope.submitRequest = function(isValid){
         var request = $scope.request;
-        $scope.submitted = true;
-        $scope.errors = false;
-        // Services.postRequest(request).then(function(data){
-        //   console.log(data);
-        //   // Wait a sec to show
-        //   $timeout(function(){ $scope.success = true; }, 1000);
-        // }, function(err){
-        //   $scope.errors = true;
-        //   $scope.submitted = false;
-        //   console.log(err);
-        // });
+        $scope.showSpinner = true;
+        $scope.msgStatus.submitted = true;
+        $scope.msgStatus.errors = false;
+        $scope.msgStatus.success = false;
+
+        if (!isValid) {
+          $scope.showSpinner = false;
+          $scope.msgStatus.errors = true;
+          $scope.msgStatus.message = "Please correct the errors above and try again.";
+          return;
+        }
+
+        console.log("Valid");
+
+        Services.postRequest(request).then(function(data){
+          console.log(data);
+          // Wait a sec to show
+          $timeout(function(){
+            $scope.msgStatus.success = true;
+            $scope.showSpinner = false;
+          }, 500);
+        }, function(err){
+          $scope.showSpinner = false;
+          $scope.msgStatus.errors = true;
+          $scope.msgStatus.message = "Server Error" + err;
+          console.log(err);
+        });
       };
     }]);
 })();
--- public/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('config', [])
-
-.constant('ENV', 'development')
-
-.constant('HOST', 'http://192.168.1.11:3000')
-
-;
