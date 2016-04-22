7bd667660b6ed2c2fdf6d7c6a6e3c0c75094d017
JJ Hiew <jj.hiew@gmail.com>
2016-04-22 01:58:55 -0400

    added analytics

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,4 +3,33 @@
   $( document ).ready(function(){
     $('.button-collapse').sideNav();
   });
+
+  $('#home-header-cta').click(function() {
+    track('Clicked CTA', { page: 'home', location: 'header' });
+  });
+
+  $('#home-demo-btn').click(function() {
+    track('Clicked Demo', { page: 'home', location: 'demo' });
+  });
+
+  $('#home-bottom-cta').click(function() {
+    track('Clicked CTA', { page: 'home', location: 'bottom' });
+  });
+
+  $('#demo-btn-1').click(function() {
+    track('Opened Demo', { page: 'demo', location: 'demo 1' });
+  });
+
+  $('#demo-btn-2').click(function() {
+    track('Opened Demo', { page: 'demo', location: 'demo 2' });
+  });
+
+  $('#send-message').click(function() {
+    track('Send Message', { page: 'contact', location: 'form' });
+  });
+
+  function track(action, data) {
+    analytics.track(action, data);
+  }
+
 })();
